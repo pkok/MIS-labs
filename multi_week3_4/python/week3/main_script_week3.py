8197c4d64153af11046d72d584044fc4c5fbb307
Patrick de Kok <patrick@cxiu.nl>
2013-11-22 22:59:23 +0100

    Part 3 works

--- multi_week3_4/python/week3/main_script_week3.py
+++ multi_week3_4/python/week3/main_script_week3.py
@@ -15,6 +15,7 @@ import math
 import random
 import sys
 import os
+import time
 
 import week3
 sys.path.insert(0, '../')
@@ -23,21 +24,27 @@ import tools
 # Went to Random.org to determine seed from uniform interval [0, 99]
 random.seed(26)
 
+"""
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # PART 1. KMEANS
+time.strftime("[%H:%M:%S] Computing part 1")
 
 # GENERATE RANDOM DATA
 x, labels = week3.generate_2d_data()
 
 week3.plot_2d_data(x, labels, None, None)
+plt.show()
 
 # PART 1. STEP 0. PICK RANDOM CENTERS
-week3.mykmeans(x,3)
+time.strftime("[%H:%M:%S] Computing part 1, step 0")
+week3.mykmeans(x, 3)
+"""
 
 """
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # PART 2. COLOR BASED IMAGE SEGMENTATION
 
+time.strftime("[%H:%M:%S] Computing part 2")
 im = Image.open('../../data/coral.jpg')
 imshow(im)
 im = np.array(im)
@@ -62,8 +69,10 @@ imshow(im_vq)
 title('K=' + str(K))
 """
 
+"""
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # PART 3. k-MEANS AND BAG-OF-WORDS
+time.strftime("[%H:%M:%S] Computing part 3")
 codebook = week3.load_codebook('../../data/codebook_100.pkl')
 K = codebook.shape[0]
 colors = week3.get_colors(K)
@@ -71,7 +80,9 @@ colors = week3.get_colors(K)
 files = os.listdir('../../data/oxford_scaled/')
 
 # PART 3. STEP 1. VISUALIZE WORDS ON IMAGES
+time.strftime("[%H:%M:%S] Computing part 3, step 1")
 word_patches = defaultdict(list)
+patch_count = defaultdict(lambda: defaultdict(list))
 files_random = random.sample(files, 5)
 
 imfolder = '../../data/oxford_scaled/'
@@ -82,18 +93,24 @@ for impath in impaths:
     [indexes, dummy] = cluster.vq(sift, codebook)  # VECTOR QUANTIZE SIFT TO WORDS
 
     # VISUALIZE WORDS
-    word_patches = week3.show_words_on_image(impath,
-                                             K,
-                                             frames,
-                                             sift,
-                                             indexes,
-                                             colors,
-                                             word_patches)
+    patch_count[impath] = week3.show_words_on_image(impath,
+                                                    K,
+                                                    frames,
+                                                    sift,
+                                                    indexes,
+                                                    colors,
+                                                    patch_count[impath])
+    patch_count[impath].default_factory = int
+    for key in patch_count[impath]:
+        word_patches[key] += patch_count[impath][key]
+        patch_count[impath][key] = len(patch_count[impath][key])
 
 # PART 3. STEP 2. PLOT COLORBAR
+time.strftime("[%H:%M:%S] Computing part 3, step 2")
 week3.get_colorbar(colors)
 
 # PART 3. STEP 3. PLOT WORD CONTENTS
+time.strftime("[%H:%M:%S] Computing part 3, step 3")
 for k in random.sample(range(len(word_patches)), 5):
     WN = len(word_patches[k])
     plt.figure()
@@ -108,9 +125,24 @@ for k in random.sample(range(len(word_patches)), 5):
         plt.axis('off')
 
 
-"""
 # PART 4. BAG-OF-WORDS IMAGE REPRESENTATION
 # USE THE np.bincount COUNTING THE INDEXES TO COMPUTE THE BAG-OF-WORDS REPRESENTATION,
+time.strftime("[%H:%M:%S] Computing part 4")
+for image in patch_count:
+    max_key = max(patch_count[image])
+    histogram = [patch_count[image][i] for i in range(max_key + 1)]
+    plt.figure()
+    plt.suptitle('Word histogram of image ' + image.split(os.sep)[-1])
+    labels = np.arange(0, len(histogram), 10)
+    ax = plt.axes()
+    ax.set_xticks(labels)
+    ax.set_ylabel("Word frequency")
+    ax.set_xlabel("Word id")
+    bars = plt.bar(np.arange(len(histogram)),
+                   histogram, 1.0, color='gray', edgecolor='none')
+    for bar, color in zip(bars, colors):
+        bar.set_color(color)
+"""
 
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # PART 5. PERFORM RETRIEVAL WITH THE BAG-OF-WORDS MODEL
@@ -130,4 +162,3 @@ prec5 = week3.precision_at_N(0, gt_labels, ranking, 5)
 
 # PART 5. STEP 4. IMPLEMENT & COMPUTE AVERAGE PRECISION
 
-"""
--- multi_week3_4/python/week3/week3.py
+++ multi_week3_4/python/week3/week3.py
@@ -9,6 +9,11 @@ from matplotlib import cm
 import matplotlib.colors as cols
 import pickle
 import random
+
+sys.path.insert(0, '../')
+import tools
+
+
 ##############################################################################
 ## YOUR IMPLEMENTATIONS
 ##############################################################################
@@ -18,7 +23,7 @@ def mykmeans(x, K):
     # PART 1. STEP 0. PICK RANDOM CENTERS
     for it in range(max_iter):
         means = np.array(random.sample(x, K))
-        print means
+        #print means
         plot_2d_data(x, None, None, means)
 
         # PART 1. STEP 1. CALCULATE DISTANCE FROM DATA TO CENTERS
@@ -27,7 +32,7 @@ def mykmeans(x, K):
             for j in np.arange(0, x.shape[0]):
                 dist[i, j] = math.sqrt((means[i][0] - x[j][0])*(means[i][0] - x[j][0]) + (means[i][1] - x[j][1])*(means[i][1] - x[j][1])) # COMPUT EUCLIDEAN DISTANCE
 
-        print dist
+        #print dist
 
         # # PART 1. STEP 2. FIND WHAT IS THE CLOSEST CENTER PER POINT
         closest = np.zeros(x.shape[0])
@@ -40,7 +45,7 @@ def mykmeans(x, K):
                     argmin = i
             closest[j] = argmin
 
-        plot_2d_data(x, None, closest, means)
+        #plot_2d_data(x, None, closest, means)
 
         # # PART 1. STEP 3. UPDATE CENTERS
         for i in np.arange(0, K):
@@ -53,11 +58,12 @@ def mykmeans(x, K):
                     center_y += x[j][1]
                     num += 1
             means[i,:] = [center_x/num, center_y/num]
-        plot_2d_data(x, None, closest, means)
-    
-    # ...
-    return codebook
-    
+        #plot_2d_data(x, None, closest, means)
+
+    return means
+    # Gave an error (codebook does not occur before...) -P
+    #return codebook
+
 
 ##############################################################################
 ### ALREADY IMPLEMENTED, DO NOT TOUCH
@@ -74,7 +80,7 @@ def get_oxford_filedata():
         filenames.append(temp[0])
         labels.append(int(temp[1]))
         label_names.append(temp[2])
-                
+
     return filenames, labels, label_names
 
 def load_codebook(path):
@@ -99,45 +105,44 @@ def generate_2d_data():
     x1 = np.random.rand(30, 2)
     x1[:, 0] = 2 * x1[:, 0] + 3
     x1[:, 1] = 4 * x1[:, 1] + 7
-    
+
     x2 = np.random.rand(30, 2)
     x2[:, 0] = 5 * x2[:, 0] + 3
     x2[:, 1] = 4 * x2[:, 1] + 2
-    
+
     x3 = np.random.rand(30, 2)
     x3[:, 0] = 5 * x3[:, 0] + 7
     x3[:, 1] = 5 * x3[:, 1] + 5
-    
+
     x = np.vstack([x1, x2, x3])
-    labels = np.hstack([0 * np.ones(30), 1 * np.ones(30), 2 * np.ones(30)])    
+    labels = np.hstack([0 * np.ones(30), 1 * np.ones(30), 2 * np.ones(30)])
     return x, labels
-    
+
 def plot_2d_data(x, labels, assignments, centers):
     colors = cm.Set1(np.linspace(0, 1, 10))
-    
-    figure    
+
+    figure
     if labels is not None:
         for i, c in zip(set(labels), colors):
             i = int(i)
             plot(x[labels == i, 0], x[labels == i, 1], 's', markeredgecolor=c)
     else:
         plot(x[:, 0], x[:, 1], 'ks')
-    
+
     if centers is not None:
         for i, c in zip(np.arange(centers.shape[0]), colors):
             i = int(i)
             plot(centers[i, 0], centers[i, 1], 'x', markeredgecolor=c, markersize=20, markeredgewidth=3)
-            
+
     if assignments is not None:
         for i, c in zip(set(assignments), colors):
             i = int(i)
             plot(x[assignments == i, 0], x[assignments == i, 1], 'o', markeredgecolor=c, markersize=12, markerfacecolor='none', markeredgewidth=2)
-    show()
 
 def get_colors(K):
     colors = np.random.rand(K, 3)
     return colors
-    
+
 def get_colorbar(colors):
     K = colors.shape[0]
     figure(figsize=(3.5, 8))
@@ -153,35 +158,35 @@ def show_words_on_image(impath, K, frames, sift, indexes, colors, word_patches):
     im = Image.open(impath)
     imshow(im)
     axis('off')
-    for k in range(K):        
+    for k in range(K):
         ix = indexes == k
         plot(frames[ix, 0], frames[ix, 1], marker='o', markeredgecolor=colors[k, :], markerfacecolor=colors[k, :], markersize=4, linewidth=0.0)
-        
+
         indexes_range = [i for i,x in enumerate(indexes) if x == k]
-        indexes_range = indexes_range[0 : max(len(indexes_range, 10))]
-        
+        indexes_range = indexes_range[0 : max(len(indexes_range), 10)]
+
         for i in indexes_range:
             r = frames[i, 1]
             c = frames[i, 0]
             pix = frames[i, 2]
-            
+
             from_row = max(0, r-pix-1)
             to_row = min(size(im, 1), r+pix+1)
             from_col = max(0, c-pix-1)
             to_col = min(size(im, 0), c+pix+1)
-            
+
             patch = np.array(im)[from_row:to_row, from_col:to_col, :]
             if patch.shape[0] == 0 or patch.shape[1] == 0:
                 continue
             word_patches[k].append(patch)
-            
+
     return word_patches
 
 def precision_at_N(query_id, gt_labels, ranking, N):
     gt_labels_N = [ gt_labels[i] for i in ranking]
     gt_labels_N = np.array(gt_labels_N[0 : N])
     gt_labels_query = gt_labels[query_id]
-    
+
     prec = float(sum(gt_labels_N == gt_labels_query)) / float(N)
     return prec
     return prec
@@ -193,7 +198,7 @@ def compute_sift(impath, edge_thresh = 10, peak_thresh = 5):
     filpat1, filnam1, filext1 = tools.fileparts(impath)
     temp_im1 = 'tmp_' + filnam1 + '.pgm'
     im1.save(temp_im1)
-    
+
     import struct
     is_64bit = struct.calcsize('P') * 8 == 64
     if platform.system() == 'Windows' and is_64bit == True:
@@ -208,13 +213,13 @@ def compute_sift(impath, edge_thresh = 10, peak_thresh = 5):
     elif platform.system() == 'Darwin':
         sift_exec = '..//..//external//vlfeat-0.9.17//bin//maci64//sift'
         command = sift_exec + ' \"' + os.getcwd() + '//' + temp_im1 + '\" --output \"' + os.getcwd() + '//' + filnam1 + '.sift.output' + '\" ' + params
-        
+
     os.system(command)
     frames, sift = read_sift_from_file(filnam1 + '.sift.output')
     os.remove(temp_im1)
     os.remove(filnam1 + '.sift.output')
     return frames, sift
-    
+
 def read_sift_from_file(sift_path):
     f = np.loadtxt(sift_path)
     return f[:, :4], f[:, 4:]
